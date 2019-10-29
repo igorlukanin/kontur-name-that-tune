dea0b6ad717bea923dfd96fdcd5fbfca7e13d421
golendukhin <golendukhin@skbkontur.ru>
2019-10-29 23:32:53 +0500

    промежуточный коммит

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const emailInput = document.querySelector('.js-email-input');
     const nameInput = document.querySelector('.js-name-input');
     const form = document.querySelector('.js-form');
+    const formFrame = document.querySelector('.js-frame_registration');
 
     let currentUser = {};
 
@@ -10,6 +11,10 @@ document.addEventListener('DOMContentLoaded', function () {
         e.preventDefault();
         currentUser = data;
         form.reset();
+        formFrame.classList.toggle('hidden');
+
+        const game = new Game(currentUser, {});
+        game.showRules();
     };
 
     submitRegistrationForm.addEventListener('click', function (e) {
@@ -18,5 +23,26 @@ document.addEventListener('DOMContentLoaded', function () {
             email: emailInput.value,
         };
         emailInput.validity.valid && nameInput.validity.valid ? submitForm(e, data) : null;
-    })
+    });
+
+    class Game {
+        constructor(user, config) {
+            this.user = user;
+            this.config = config;
+            this.rulesFrame = document.querySelector('.js-frame_rules');
+            this.startGameBtn = document.querySelector('.js-start-game');
+
+            this.startGameBtn.addEventListener('click', () => {
+                this.rulesFrame.classList.toggle('hidden');
+                this.run();
+            })
+        }
+
+        run() {
+            console.log('start game')
+        }
+        showRules() {
+            this.rulesFrame.classList.toggle('hidden');
+        }
+    }
 });
